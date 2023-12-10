7df686ade774200ca878275f02640c1c55b13e8c
Santiago Figueroa <santiagofig00@gmail.com>
2023-12-10 16:08:58 -0300

    Agrego logica de CRUD para Proveedor

---
+++ 1ER-ENTREGABLE/assets/js/classProducto.js
@@ -0,0 +1,10 @@
+export class Proveedor{
+    constructor(codigo, nombreProd, categoria, proveedor, precio, descripcion){
+        this.codigo = codigo;
+        this.nombreProd = nombreProd;
+        this.categoria = categoria;
+        this.proveedor = proveedor;
+        this.precio = precio;
+        this.descripcion = descripcion;
+    }
+}
